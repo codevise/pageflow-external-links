5db86c6d3fe441b22b2302255397965656d6f8c5
Tim Fischbach <tfischbach@codevise.de>
2014-10-10 21:06:04 +0200

    use file thumbnail helper for sites

--- app/helpers/pageflow/external_links/sites_helper.rb
+++ app/helpers/pageflow/external_links/sites_helper.rb
@@ -4,6 +4,17 @@ module Pageflow
       def external_links_site_css_class(site)
         ['link-item', site.title.blank? && site.description.blank? ? 'no_text' : ''].compact.join(' ')
       end
+
+
+      def external_links_site_thumbnail_css_class(site)
+        classes = ['link-thumbnail']
+
+        if file = site.thumbnail_file
+          classes << file_thumbnail_css_class(file, :link_thumbnail_large)
+        end
+
+        classes.join(' ')
+      end
     end
   end
 end
--- app/models/pageflow/external_links/site.rb
+++ app/models/pageflow/external_links/site.rb
@@ -2,6 +2,10 @@ module Pageflow
   module ExternalLinks
     class Site < ActiveRecord::Base
       include RevisionComponent
+
+      def thumbnail_file
+        ImageFile.find_by_id(thumbnail)
+      end
     end
   end
 end
