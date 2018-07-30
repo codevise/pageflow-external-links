c72850abbf161a84c60b81e386eec67020f376f2
Tim Fischbach <tfischbach@codevise.de>
2018-07-30 14:28:47 +0200

    Upgrade to Rails 5
    Depend on Pageflow 13 and update migration syntax.

--- db/migrate/20141218183701_change_site_url_to_text.rb
+++ db/migrate/20141218183701_change_site_url_to_text.rb
@@ -1,4 +1,4 @@
-class ChangeSiteUrlToText < ActiveRecord::Migration
+class ChangeSiteUrlToText < ActiveRecord::Migration[4.2]
   def change
     change_column(:pageflow_external_links_sites, :url, :text)
   end
