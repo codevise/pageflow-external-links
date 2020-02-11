72d62d54f89c759e2435b8157f1a179572e0e713
Tim Fischbach <tfischbach@codevise.de>
2020-02-11 12:43:43 +0100

    Prevent error with missing site urls in Pageflow 15.1
    Since the extraction of the paged entry type into a separate engine,
    calling `link_to` with nil for a URL causes an error. Rails tries to
    turn an empty hash into a route. This no longer works inside the paged
    engine, since it only contains editor related routes.
    
    As a workaround, default to an empty string. This leads to the same
    behavior as before: Creating a link to the current location.
    
    Extend CI setup to make sure the site renders in cases like this.
    
    REDMINE-17412

--- spec/integration/page_type_spec.rb
+++ spec/integration/page_type_spec.rb
@@ -2,3 +2,49 @@ require 'spec_helper'
 require 'pageflow/lint'
 
 Pageflow::Lint.page_type(Pageflow::ExternalLinks.page_type)
+
+module Pageflow
+  module ExternalLinks
+    describe 'built in video page type', type: :helper do
+      include RenderPageTestHelper
+
+      it 'renders links to referenced sites' do
+        revision = FactoryBot.create(:revision, :published)
+        Site.create!(revision: revision,
+                     perma_id: 10,
+                     url: 'https://example.com',
+                     title: 'Example')
+
+        page = FactoryBot.create(:page,
+                                 template: 'external_links',
+                                 revision: revision,
+                                 configuration: {
+                                   linked_external_site_perma_ids: [10]
+                                 })
+
+        html = render_page(page)
+
+        expect(html).to have_selector('.link-title', text: 'Example')
+      end
+
+      it 'can handle sites with missing url' do
+        revision = FactoryBot.create(:revision, :published)
+        Site.create!(revision: revision,
+                     perma_id: 10,
+                     url: nil,
+                     title: 'Example')
+
+        page = FactoryBot.create(:page,
+                                 template: 'external_links',
+                                 revision: revision,
+                                 configuration: {
+                                   linked_external_site_perma_ids: [10]
+                                 })
+
+        html = render_page(page)
+
+        expect(html).to have_selector('.link-title', text: 'Example')
+      end
+    end
+  end
+end
